7447d05ddef7692efba6b47caca2ab739cfcceeb
jaeil <59818827+jaeilssss@users.noreply.github.com>
2021-06-04 14:45:01 +0900

    indicator linear layout 기

--- viewPager2Project/app/src/main/java/com/example/viewpager2project/MainActivity.kt
+++ viewPager2Project/app/src/main/java/com/example/viewpager2project/MainActivity.kt
@@ -3,14 +3,16 @@ package com.example.viewpager2project
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.viewpager2.widget.CompositePageTransformer
-import androidx.viewpager2.widget.MarginPageTransformer
+import androidx.core.content.ContextCompat
 import androidx.viewpager2.widget.ViewPager2
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    var layoutOnBoardingIndicators : LinearLayout ?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -26,18 +28,67 @@ class MainActivity : AppCompatActivity() {
         viewpager.getChildAt(0).overScrollMode=View.OVER_SCROLL_NEVER
         viewpager.adapter = adater
 
-        var transform = CompositePageTransformer()
-        transform.addTransformer(MarginPageTransformer(8))
 
-        transform.addTransformer(ViewPager2.PageTransformer{ view: View, fl: Float ->
-            var v = 1-Math.abs(fl)
-            view.scaleY = 0.8f + v * 0.2f
+        layoutOnBoardingIndicators = findViewById(R.id.indicators)
+        setupOnBoardingIndicators()
+        setCurrentOnboardingIndicator(0)
+
+        viewpager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position : Int){
+                setCurrentOnboardingIndicator(position)
+            }
+
         })
+//        var transform = CompositePageTransformer()
+//        transform.addTransformer(MarginPageTransformer(8))
+//
+//        transform.addTransformer(ViewPager2.PageTransformer{ view: View, fl: Float ->
+//            var v = 1-Math.abs(fl)
+//            view.scaleY = 0.8f + v * 0.2f
+//        })
+//
+//        viewpager.setPageTransformer(transform)
+//
+//        var tab = findViewById<TabLayout>(R.id.tab)
+//
+//
+//        TabLayoutMediator(tab,viewpager){_,_ ->  }.attach()
+    }
 
-        viewpager.setPageTransformer(transform)
+    private fun setupOnBoardingIndicators(){
+        val indicators =
+            arrayOfNulls<ImageView>(3)
 
-        var tab = findViewById<TabLayout>(R.id.tab)
+        var layoutParams = LinearLayout.LayoutParams(
+            ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT
+        )
 
-        TabLayoutMediator(tab,viewpager){_,_ ->  }.attach()
+        layoutParams.setMargins(8,0,8,0)
+
+        for( i in indicators.indices){
+            indicators[i] = ImageView(applicationContext)
+            indicators[i]?.setImageDrawable(ContextCompat.getDrawable(
+                applicationContext,
+                R.drawable.onboarding_indicator_inactivie
+            ))
+
+            indicators[i]?.layoutParams = layoutParams
+
+            layoutOnBoardingIndicators?.addView(indicators[i])
+        }
+    }
+
+    private fun setCurrentOnboardingIndicator( index : Int){
+        var childCount = layoutOnBoardingIndicators?.childCount
+        for(i in  0 until childCount!!){
+            var imageView = layoutOnBoardingIndicators?.getChildAt(i) as ImageView
+            if(i==index){
+                imageView.setImageDrawable(ContextCompat.getDrawable(applicationContext,
+                    R.drawable.onboarding_indicator_active))
+            }else{
+                imageView.setImageDrawable(ContextCompat.getDrawable(applicationContext,
+                    R.drawable.onboarding_indicator_inactivie))
+            }
+        }
     }
 }
