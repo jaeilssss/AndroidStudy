4781905ead85a701a76434c3c8a33e36fb0ffc6c
jaeil <59818827+jaeilssss@users.noreply.github.com>
2021-06-08 17:46:26 +0900

    구현완료

--- AlarmProject/app/src/main/java/com/example/alarmproject/AlarmReceiver.kt
+++ AlarmProject/app/src/main/java/com/example/alarmproject/AlarmReceiver.kt
@@ -10,44 +10,44 @@ import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 
-class AlarmReceiver:BroadcastReceiver(){
-
+class AlarmReceiver: BroadcastReceiver() {
 
     companion object {
-        const val NOTIFICATION_CHANNEL_ID = "1000"
         const val NOTIFICATION_ID = 100
-     }
-    override fun onReceive(context: Context, intent: Intent?) {
-
-        createNotificationChannel(context)
-        notifiyNotification(context)
+        const val NOTIFICATION_CHANNEL_ID = "1000"
     }
 
-    private fun createNotificationChannel(context:Context){
+    override fun onReceive(context: Context, intent: Intent) {
+        createNotificationChannel(context)
+        notifyNotification(context)
+    }
 
-        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+    private fun createNotificationChannel(context: Context) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val notificationChannel = NotificationChannel(
-                    "1000",
+                    NOTIFICATION_CHANNEL_ID,
                     "기상 알람",
                     NotificationManager.IMPORTANCE_HIGH
             )
+
             NotificationManagerCompat.from(context).createNotificationChannel(notificationChannel)
         }
-
-
     }
 
-    private fun notifiyNotification(context: Context){
 
-        with(NotificationManagerCompat.from(context)){
-            val build = NotificationCompat.Builder(context,NOTIFICATION_CHANNEL_ID)
+    private fun notifyNotification(context: Context) {
+        with(NotificationManagerCompat.from(context)) {
+            val build = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
                     .setContentTitle("알람")
-                    .setContentText("일어날 시간입니다!")
+                    .setContentText("일어날 시간입니다.")
                     .setSmallIcon(R.drawable.ic_launcher_foreground)
                     .setPriority(NotificationCompat.PRIORITY_HIGH)
-                notify(NOTIFICATION_ID,build.build())
-        }
-    }
 
+            notify(NOTIFICATION_ID, build.build())
+
+        }
+
+    }
 
 }
