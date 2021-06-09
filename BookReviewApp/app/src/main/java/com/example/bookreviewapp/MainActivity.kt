857da830407630b2fe1c2ff4cb7572f2bc6599ef
jaeil <59818827+jaeilssss@users.noreply.github.com>
2021-06-09 12:47:26 +0900

    Retrofit 구현

--- BookReviewApp/app/src/main/java/com/example/bookreviewapp/MainActivity.kt
+++ BookReviewApp/app/src/main/java/com/example/bookreviewapp/MainActivity.kt
@@ -2,10 +2,60 @@ package com.example.bookreviewapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.bookreviewapp.api.BookService
+import com.example.bookreviewapp.model.BestSellerDto
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.Retrofit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://book.interpark.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val bookService = retrofit.create(BookService::class.java)
+
+        bookService.getBestSellerBooks("F8E7CACAE6156007B5E0A5B40CDF46B1BEB38AB563A5171300B90ADB8C94F6AA")
+            .enqueue(object : Callback<BestSellerDto>{
+                override fun onResponse(
+                    call: Call<BestSellerDto>,
+                    response: Response<BestSellerDto>
+                ) {
+                    //성공 처리
+
+                    if(response.isSuccessful.not()){
+                        Log.e(TAG,"NOT Success")
+                        return
+                    }
+
+                    response.body()?.let {
+                        Log.d(TAG,it.toString())
+                        it.books.forEach { book->
+                            Log.d(TAG,book.toString())
+                        }
+                    }
+                    }
+
+                override fun onFailure(call: Call<BestSellerDto>, t: Throwable) {
+                        // 실패처리
+
+                    Log.e(TAG,t.toString())
+                    }
+
+            })
+
+
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
