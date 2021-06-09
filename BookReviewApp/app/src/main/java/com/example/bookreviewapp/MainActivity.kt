1a2810a8efb21b7d6fabb59e00c5991dcbb945fa
jaeil <59818827+jaeilssss@users.noreply.github.com>
2021-06-09 17:11:27 +0900

    검색 기능 구현 but 검색할시 색갈이 좀 변함..

--- BookReviewApp/app/src/main/java/com/example/bookreviewapp/MainActivity.kt
+++ BookReviewApp/app/src/main/java/com/example/bookreviewapp/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.bookreviewapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.KeyEvent
+import android.view.MotionEvent
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.bookreviewapp.adapter.BookAdapter
 import com.example.bookreviewapp.api.BookService
 import com.example.bookreviewapp.databinding.ActivityMainBinding
 import com.example.bookreviewapp.model.BestSellerDto
+import com.example.bookreviewapp.model.SearchBookDto
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -17,7 +20,7 @@ import retrofit2.Retrofit
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var bookService : BookService
     private lateinit var adapter: BookAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,9 +34,9 @@ class MainActivity : AppCompatActivity() {
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
-        val bookService = retrofit.create(BookService::class.java)
+         bookService = retrofit.create(BookService::class.java)
 
-        bookService.getBestSellerBooks("F8E7CACAE6156007B5E0A5B40CDF46B1BEB38AB563A5171300B90ADB8C94F6AA")
+        bookService.getBestSellerBooks(getString(R.string.interParkAPIKey))
             .enqueue(object : Callback<BestSellerDto>{
                 override fun onResponse(
                     call: Call<BestSellerDto>,
@@ -64,7 +67,43 @@ class MainActivity : AppCompatActivity() {
 
             })
 
+        binding.searchEditText.setOnKeyListener { view, keyCode, keyEvent ->
 
+            if(keyCode==KeyEvent.KEYCODE_ENTER && keyEvent.action == MotionEvent.ACTION_DOWN){
+                    search(binding.searchEditText.text.toString())
+                // setOnKeyListener는 반환값이 있어야한다!
+                return@setOnKeyListener true
+            }
+
+            return@setOnKeyListener false
+        }
+
+    }
+
+    private fun search(keyword : String){
+        bookService.getBooksByName(getString(R.string.interParkAPIKey),keyword)
+             .enqueue(object : Callback<SearchBookDto>{
+            override fun onResponse(
+                call: Call<SearchBookDto>,
+                response: Response<SearchBookDto>
+            ) {
+                //성공 처리
+
+                if(response.isSuccessful.not()){
+                    Log.e(TAG,"NOT Success")
+                    return
+                }
+                adapter.submitList(response.body()?.books.orEmpty())
+
+            }
+
+            override fun onFailure(call: Call<SearchBookDto>, t: Throwable) {
+                // 실패처리
+
+                Log.e(TAG,t.toString())
+            }
+
+        })
     }
 
     fun initBookRecyclerView(){
