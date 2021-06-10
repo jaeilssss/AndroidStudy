b2fdb32bb542be2367fda36d5da656f0210bc764
jaeil <59818827+jaeilssss@users.noreply.github.com>
2021-06-10 12:29:34 +0900

    customAlertDialog init

--- BookReviewApp/app/src/main/java/com/example/bookreviewapp/MainActivity.kt
+++ BookReviewApp/app/src/main/java/com/example/bookreviewapp/MainActivity.kt
@@ -5,11 +5,15 @@ import android.os.Bundle
 import android.util.Log
 import android.view.KeyEvent
 import android.view.MotionEvent
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.Room
 import com.example.bookreviewapp.adapter.BookAdapter
+import com.example.bookreviewapp.adapter.HistoryAdapter
 import com.example.bookreviewapp.api.BookService
 import com.example.bookreviewapp.databinding.ActivityMainBinding
 import com.example.bookreviewapp.model.BestSellerDto
+import com.example.bookreviewapp.model.History
 import com.example.bookreviewapp.model.SearchBookDto
 import retrofit2.Call
 import retrofit2.Callback
@@ -22,12 +26,22 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var bookService : BookService
     private lateinit var adapter: BookAdapter
+    private lateinit var db : AppDatabase
+    private lateinit var historyAdapter : HistoryAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         initBookRecyclerView()
+
+        initHistoryRecyclerView()
+        db  = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java,
+            "BookSearchDB"
+        ).build()
 
         val retrofit = Retrofit.Builder()
             .baseUrl("https://book.interpark.com")
@@ -48,7 +62,6 @@ class MainActivity : AppCompatActivity() {
                         Log.e(TAG,"NOT Success")
                         return
                     }
-
                     response.body()?.let {
                         Log.d(TAG,it.toString())
                         it.books.forEach { book->
@@ -58,7 +71,6 @@ class MainActivity : AppCompatActivity() {
                         adapter.submitList(it.books)
                     }
                     }
-
                 override fun onFailure(call: Call<BestSellerDto>, t: Throwable) {
                         // 실패처리
 
@@ -67,16 +79,7 @@ class MainActivity : AppCompatActivity() {
 
             })
 
-        binding.searchEditText.setOnKeyListener { view, keyCode, keyEvent ->
 
-            if(keyCode==KeyEvent.KEYCODE_ENTER && keyEvent.action == MotionEvent.ACTION_DOWN){
-                    search(binding.searchEditText.text.toString())
-                // setOnKeyListener는 반환값이 있어야한다!
-                return@setOnKeyListener true
-            }
-
-            return@setOnKeyListener false
-        }
 
     }
 
@@ -88,6 +91,8 @@ class MainActivity : AppCompatActivity() {
                 response: Response<SearchBookDto>
             ) {
                 //성공 처리
+               hideHistoryView()
+                saveSearchKeyword(keyword)
 
                 if(response.isSuccessful.not()){
                     Log.e(TAG,"NOT Success")
@@ -96,21 +101,77 @@ class MainActivity : AppCompatActivity() {
                 adapter.submitList(response.body()?.books.orEmpty())
 
             }
-
             override fun onFailure(call: Call<SearchBookDto>, t: Throwable) {
                 // 실패처리
-
+                hideHistoryView()
                 Log.e(TAG,t.toString())
             }
 
         })
     }
 
+
+    private fun initSearchEditText(){
+        binding.searchEditText.setOnTouchListener { view, motionEvent ->
+
+            if(motionEvent.action == MotionEvent.ACTION_DOWN){
+                showHistoryView()
+                return@setOnTouchListener true
+            }
+            return@setOnTouchListener false
+        }
+        binding.searchEditText.setOnKeyListener { view, keyCode, keyEvent ->
+
+            if(keyCode==KeyEvent.KEYCODE_ENTER && keyEvent.action == MotionEvent.ACTION_DOWN){
+                search(binding.searchEditText.text.toString())
+                // setOnKeyListener는 반환값이 있어야한다!
+                return@setOnKeyListener true
+            }
+
+            return@setOnKeyListener false
+
+        }
+    }
     fun initBookRecyclerView(){
          adapter = BookAdapter()
         binding.bookRecyclerView.layoutManager = LinearLayoutManager(this)
         binding.bookRecyclerView.adapter = adapter
 
+    }
+
+    private fun initHistoryRecyclerView(){
+        historyAdapter = HistoryAdapter(historyDeleteClickedListener = {
+            deleteSearchKeyword(it)
+        })
+
+        binding.historyRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.historyRecyclerView.adapter = historyAdapter
+        initSearchEditText()
+    }
+    private fun showHistoryView(){
+        Thread{
+            val keywords = db.historyDao().getAll().reversed()
+            runOnUiThread{
+                binding.historyRecyclerView.isVisible=true
+                historyAdapter.submitList(keywords.orEmpty())
+            }
+        }
+    }
+
+    private fun hideHistoryView(){
+        binding.historyRecyclerView.isVisible = false
+    }
+    private fun saveSearchKeyword(keyword: String){
+        Thread{
+            db.historyDao().insertHistory(History(null,keyword))
+        }.start()
+    }
+    private fun deleteSearchKeyword(keyword: String){
+        Thread{
+            db.historyDao().delete(keyword)
+            showHistoryView()
+            //todo view 갱신
+        }
     }
     companion object {
         private const val TAG = "MainActivity"
---
+++ BookReviewApp/app/src/main/java/com/example/bookreviewapp/adapter/HistoryAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.bookreviewapp.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.bookreviewapp.databinding.ItemHistoryBinding
+import com.example.bookreviewapp.model.History
+
+class HistoryAdapter(val historyDeleteClickedListener : (String)->(Unit)) : ListAdapter<History, HistoryAdapter.HistoryItemViewHolder>(diffUtil){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HistoryItemViewHolder {
+        return HistoryItemViewHolder(ItemHistoryBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+    }
+
+    override fun onBindViewHolder(holder: HistoryItemViewHolder, position: Int) {
+
+    }
+    inner class HistoryItemViewHolder(private val binding : ItemHistoryBinding): RecyclerView.ViewHolder(binding.root){
+        fun binding(historyModel : History){
+            binding.historyKeywordTextView.text = historyModel.keyword
+            binding.historyKeywordDeleteButton.setOnClickListener {
+                historyDeleteClickedListener(historyModel.keyword.orEmpty())
+            }
+        }
+    }
+
+    companion object{
+        val diffUtil = object  : DiffUtil.ItemCallback<History>(){
+            override fun areItemsTheSame(oldItem: History, newItem: History): Boolean {
+                return oldItem ==newItem
+            }
+
+            override fun areContentsTheSame(oldItem: History, newItem: History): Boolean {
+                return oldItem.keyword == newItem.keyword
+            }
+
+        }
+    }
+}
